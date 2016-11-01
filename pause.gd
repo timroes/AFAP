db91d8548c8eb3799ac6eacc1fd556b3b2a26c44
Tim Roes <mail@timroes.de>
2016-11-01 19:23:47 +0100

    Enable pause on unfocus

--- pause.gd
+++ pause.gd
@@ -1,5 +1,7 @@
 extends CanvasLayer
 
+export(bool) var pause_on_unfocus = true
+
 onready var overlay = get_node("pause_overlay")
 
 func _ready():
@@ -8,8 +10,20 @@ func _ready():
 func _input(event):
 	if event.is_action_pressed("pause"):
 		if get_tree().is_paused():
-			get_tree().set_pause(false)
-			overlay.hide()
+			unpause()
 		else:
-			get_tree().set_pause(true)
-			overlay.show()
+			pause()
+			
+func _notification(what):
+	# Pause game, when window loses focus
+	if what == MainLoop.NOTIFICATION_WM_FOCUS_OUT:
+		pause()
+	pass
+
+func pause():
+	get_tree().set_pause(true)
+	overlay.show()
+	
+func unpause():
+	get_tree().set_pause(false)
+	overlay.hide()
